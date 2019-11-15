6f79a4f57e40263789cb74de3b4414d63aac036a
Andy Fiddaman <omnios@citrus-it.co.uk>
2019-11-15 08:48:29 +0000

    mdb_getopts() is missing a sentinel in places
    Change-Id: Idf4786bd3d8b714493b5c0d974c35d40951e9ca0

--- usr/src/cmd/mdb/intel/mdb/mdb_bhyve.c
+++ usr/src/cmd/mdb/intel/mdb/mdb_bhyve.c
@@ -412,7 +412,8 @@ bhyve_cpustack_dcmd(uintptr_t addr, uint_t flags, int argc,
 
 	i = mdb_getopts(argc, argv,
 	    'c', MDB_OPT_UINT64, &cpu,
-	    'v', MDB_OPT_SETBITS, 1, &verbose);
+	    'v', MDB_OPT_SETBITS, 1, &verbose,
+	    NULL);
 
 	argc -= i;
 	argv += i;
